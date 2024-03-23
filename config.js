a423d4997d10c96e04afa9af7c01633aa759f6f8
Dav-id1 <153999113+Dav-id1@users.noreply.github.com>
2024-03-23 15:45:13 +0100

    Update config.js

--- config.js
+++ config.js
@@ -13,11 +13,11 @@ global.gurl = 'king-md.db' // add your username
 global.sudo = process.env.SUDO || '923124533358'
 global.devs = '923124533358';
 global.website = 'https://github.com/arkhan998/MALIK-MD' //wa.me/+91000000000000
-global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://telegra.ph/file/dce77f7538019d05afe65.jpg'
+global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://telegra.ph/file/dcffdf049c7cf0ca9375b.jpg'
 module.exports = {
   
   sessionName:  process.env.SESSION_ID ||  "session",
-  botname:   process.env.BOT_NAME === undefined ? 'MALIK-BOT' : process.env.BOT_NAME,
+  botname:   process.env.BOT_NAME === undefined ? 'мσяту' : process.env.BOT_NAME,
   ownername: process.env.OWNER_NAME === undefined ? 'arkhan' : process.env.OWNER_NAME,
   author:  process.env.PACK_INFO === undefined ? 'bot' : process.env.PACK_INFO.split(";")[0],
   auto_read_status :  process.env.AUTO_READ_STATUS === undefined ? false : process.env.AUTO_READ_STATUS,
